081848c5f58166e19a0f9375bd2a1c08a5075cc3
Gareth Noble <garethnoble3@gmail.com>
2026-01-21 00:25:12 -0500

    chore: added postgresql database connection type protection for dev and prod

--- config/database.js
+++ config/database.js
@@ -1,31 +1,37 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-/* For local development PostgreSQL server.
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
-*/
+let pool;
 
-// Connect to external PostgreSQL databse.
-const pool = new Pool({
-    connectionString: process.env.DATABASE_PUBLIC_URL,
-    ssl: {
-        rejectUnauthorized: false 
-    }
-});
+if (process.env.DB_CONNECTION_TYPE === 'local') {
+    // For local development PostgreSQL server.
+    pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT || 5432,
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+    });
+}
+else {
+    // Connect to external PostgreSQL databse.
+    pool = new Pool({
+        connectionString: process.env.DATABASE_PUBLIC_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+
 
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1);
 });
+
 
 module.exports = pool;
